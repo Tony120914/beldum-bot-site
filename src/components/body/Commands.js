f3a4166411ef55215fe537dce22807c56d6fcf56
Tony Lin <ttony.lin@mail.utoronto.ca>
2021-10-18 22:23:04 -0400

    styled commands table

--- src/components/body/Commands.js
+++ src/components/body/Commands.js
@@ -10,19 +10,45 @@ class Commands extends React.Component {
   render() {
     return (
       <div className='Commands'>
-        <h1>Commands | Examples | Descriptions</h1>
-        <ul>
-          {commands.map(cmd => {
-            return (
-              <li key={cmd.command}>
-                {`${cmd.usage} | ${cmd.description} | ${cmd.example}`}
-              </li>
-            );
-          })}
-        </ul>
+        <div className='table-responsive' style={{width:'75%', margin:'auto'}}>
+          <table className='table table-dark table-striped table-hover'>
+            <TableHeader/>
+            <TableBody/>
+          </table>
+        </div>
       </div>
+    );
+  }
+} export default Commands;
+
+class TableHeader extends React.Component {
+  render() {
+    return (
+      <thead>
+        <tr>
+          <th className='col-3' scope='col'>Commands</th>
+          <th className='col-3' scope='col'>Examples</th>
+          <th className='col-6' scope='col'>Descriptions</th>
+        </tr>
+      </thead>
     );
   }
 }
 
-export default Commands;
+class TableBody extends React.Component {
+  render() {
+    return (
+      <tbody>
+        {commands.map(cmd => {
+          return (
+            <tr key={cmd.command}>
+              <td><code>{cmd.usage}</code></td>
+              <td><kbd>{cmd.example}</kbd></td>
+              <td>{cmd.description}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  }
+}
