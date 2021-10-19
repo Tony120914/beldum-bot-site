67b41ce0738460097dfc57af0be5fd130473eea0
Tony Lin <ttony.lin@mail.utoronto.ca>
2021-10-18 23:30:03 -0400

    hashrouter, gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
