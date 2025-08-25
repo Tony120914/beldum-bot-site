872729d3e5c7a5bba56af80dfcfd2fce7098124f
Tony Lin <tonylin120914@gmail.com>
2025-08-24 23:29:13 -0400

    viewTransition (elements fade in and out)

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from "react-router";
+import { RouterProvider } from "react-router";
 import './scss/styles.scss'
-import './css/styles.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
-import Home from './pages/Home.jsx';
-import Docs from './pages/Docs.jsx';
+import hashRouter from './hashRouter.js';
 
 const root = document.getElementById('root');
 if (!root) throw new Error("root missing in index.html");
 createRoot(root).render(
     <StrictMode>
-        <HashRouter>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path='docs'>
-                    <Route index element={<Docs />} />
-                </Route>
-            </Routes>
-        </HashRouter>
+        <RouterProvider router={hashRouter} />
     </StrictMode>,
-)
+);
